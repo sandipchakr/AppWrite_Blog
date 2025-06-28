bcdbde7584c3e2ff68ef0ad347417f88bd816ff6
sandip chakraborty <unicornsc77@gmail.com>
2025-06-28 12:17:16 +0530

    change the background color and fontt color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,7 +34,7 @@ function Header() {
     }
   ]
   return (
-    <header className="py-3 shadow bg-gray-500">
+    <header className="py-3 shadow bg-pink-800">
       <Container>
         <nav className="flex">
           <div className="mr-4">
